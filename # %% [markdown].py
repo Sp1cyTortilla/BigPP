b22366536dd6bb42fa15d8a6c43b9a41d1f0b46c
Sp1cyTortilla <145488213+Sp1cyTortilla@users.noreply.github.com>
2024-12-02 16:21:28 -0500

    Add files via upload

---
+++ # %% [markdown].py
@@ -0,0 +1,19 @@
+# %% [markdown]
+# PP Final
+# 
+# By: Jerry Taylor and Ian small-penis
+
+# %%
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets
+from sklearn.preprocessing import OneHotEncoder
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.models import Sequential
+
+# %%
+
+
+
